5990491b02a0df2ea100962d0cf4425375a06ec2
lbv_o <gromlbv@gmail.com>
2025-07-09 11:28:04 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+@app.route('/<path:path>')
+def index(path=''):
+    return render_template('index.html')
+
+@app.route('/pages/<path:filename>')
+def pages(filename):
+    return send_from_directory('static/pages', filename)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5200,debug=True)
