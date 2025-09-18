7e43b8ecc19f1d098cb6b0e24bf553f753dacfbb
lbv_o <gromlbv@gmail.com>
2025-09-18 16:54:52 +0300

    ru localisation

--- app.py
+++ app.py
@@ -1,25 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='static')
 r = render_template
-@app.route('/')
-@app.route('/<path:path>')
-def index(path='', contact=''):
-    if contact:
-        return r('index.html', contact=True)
-    return r('index.html')
 
-@app.route('/<path:category>/<path:filename>')
-def nested_pages(category, filename):
-    return r('index.html')
+def is_russian_domain():
+    host = request.headers.get('Host', '').lower()
+    return host.startswith('seniwave.ru') or request.args.get('ru') == '1' or request.args.get('en') != '1'   
+
+def get_pages_path():
+    return 'pages_ru/' if is_russian_domain() else 'pages/'
+
+@app.route('/')
+def index():
+    if is_russian_domain():
+        return r('index_ru.html', ru=True)
+    else:
+        return r('index.html')
 
 @app.route('/pages/<path:filename>')
 def pages(filename):
-    return r('pages/' + filename)
+    pages_path = get_pages_path()
+    return r(pages_path + filename)
+
+@app.route('/<path:path>')
+def catch_all(path):
+    if is_russian_domain():
+        return r('index_ru.html', ru=True)
+    else:
+        return r('index.html')
     
 @app.route('/contact')
 def contact():
-    return r('index.html', contact=True)
+    if is_russian_domain():
+        return r('index_ru.html', contact=True, ru=True)
+    else:
+        return r('index.html', contact=True, ru=is_russian_domain())
 
 
 if __name__ == '__main__':
